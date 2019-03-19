d61e7263fdfe06b8947d86f03482c9661e424e19
Dima <goncharovdma@gmail.com>
2019-03-19 10:45:22 +0100

    Add django-rest + swagger + test_app

---
+++ backend/api/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from api.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for USERS
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
