5072bcb072213482141e02536d8b94288bb7eb94
lexk <kutsenko.new@gmail.com>
2019-03-29 20:01:07 +0200

    fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,5 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from api.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
