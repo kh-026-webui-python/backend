8ff40a429f4cd7b0fa4d16d4b41435aaab807d07
fronimus <44166364+fronimus@users.noreply.github.com>
2019-03-29 20:32:58 +0200

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from api.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import psycopg2
@@ -13,7 +12,6 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint for USERS
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
 
 
 class HealthCheckView(APIView):
