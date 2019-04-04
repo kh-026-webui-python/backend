eebbf0e192b8572ef44d8431386479f12ad11b5d
Andrew Babenko <andruonline11@gmail.com>
2019-04-04 14:03:45 +0300

    Revert "Revert "Upload file""

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
-from api.serializers import UserSerializer
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework.views import APIView
+from rest_framework.parsers import FileUploadParser
+from api.serializers import UserSerializer
+
 import psycopg2
 import os
 
@@ -13,6 +19,31 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = User
+
+
+@csrf_exempt
+def upload_file(request):
+    if request.method == 'POST':
+
+        filename = request.FILES['file'].name
+
+        if filename.endswith('.csv'):
+
+            '''
+            TODO: add file saving here
+            '''
+
+            return JsonResponse({'message': 'Sent'}, status=status.HTTP_200_OK)
+
+        else:
+
+            return JsonResponse({'message': 'Wrong extension, please use .csv files in your request'},
+                                status=status.HTTP_409_CONFLICT)
+    else:
+
+        return JsonResponse({'message': 'Wrong method,use POST'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class HealthCheckView(APIView):
@@ -32,6 +63,7 @@ class HealthCheckView(APIView):
         return Response({"server": "pong",
                          "database": db})
 
-def upload_CV(request):
-    if request.method == 'POST' and request.FILES['file']:
-        myfile = request.FILES['file']
+
+# def upload_CV(request):
+#    if request.method == 'POST' and request.FILES['file']:
+#       myfile = request.FILES['file']
